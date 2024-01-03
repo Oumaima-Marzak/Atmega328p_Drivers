3f3e5d90eee8aa5719d88f5e5ef9f04d8d30ecb0
Oumaima <freelancework.oumaima@gmail.com>
2024-01-03 22:13:09 +0000

    V1

--- lib/DataTypes.h
+++ lib/DataTypes.h
@@ -1,1 +1,17 @@
-ifndef DATATYPES_H
+/*
+*   Author : Oumaima MARZAK
+*   
+*
+*
+*
+*/
+
+#ifndef DATATYPES_H
+#define DATATYPES_H
+
+typedef unsigned char           U8;   // 1 byte >> 8 bits
+typedef unsigned short int      U16;  // 2 byte >> 16 bits
+typedef unsigned long int       U32;  // 4 byte >> 32 bits
+typedef unsigned long long int  U64;  // 8 byte >> 64 bits
+
+#endif
