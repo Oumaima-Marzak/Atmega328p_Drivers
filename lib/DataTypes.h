92bdecf5b09ae07fe71438095981749fa83f31d9
Oumaima <freelancework.oumaima@gmail.com>
2024-01-04 21:26:02 +0000

    V1 changing data names

--- lib/DataTypes.h
+++ lib/DataTypes.h
@@ -41,9 +41,9 @@
 #ifndef DATATYPES_H
 #define DATATYPES_H
 
-typedef unsigned char           U8;   // 1 byte >> 8 bits
-typedef unsigned short int      U16;  // 2 byte >> 16 bits
-typedef unsigned long int       U32;  // 4 byte >> 32 bits
-typedef unsigned long long int  U64;  // 8 byte >> 64 bits
+typedef unsigned char           Unt8;   // 1 byte >> 8 bits
+typedef unsigned short int      Unt16;  // 2 byte >> 16 bits
+typedef unsigned long int       Unt32;  // 4 byte >> 32 bits
+typedef unsigned long long int  Unt64;  // 8 byte >> 64 bits
 
 #endif
