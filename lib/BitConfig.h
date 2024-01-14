c8143ed64b6b0dc27a6a45a2bb76d18e40b832fa
Oumaima <freelancework.oumaima@gmail.com>
2024-01-14 00:50:07 +0000

    First Comments Update

--- lib/BitConfig.h
+++ lib/BitConfig.h
@@ -46,10 +46,10 @@
 #ifndef BITCONFIG_H
 #define BITCONFIG_H
 
-#define SET_VAL_BIT(REG , BIT)      REG |= (1 << BIT)        // Set the bit value to 1
-#define CLEAR_VAL_BIT(REG , BIT)    REG &= (~(1 << BIT))     // Clear the bit value to 0
-#define TOGGLE_VAL_BIT(REG , BIT)   REG ^= (1 << BIT)        // Toggle the bit value
-#define READ_BIT_VAL(REG , BIT)     REG  & (1 << BIT)        // Read the bit value that might be 0 or 1 
+#define SET_VAL_BIT(REG , BIT)      REG |= (1 << BIT)        // Set the bit value (BIT) in the register (REG) to 1
+#define CLEAR_VAL_BIT(REG , BIT)    REG &= (~(1 << BIT))     // Clear the bit value (BIT) in the register (REG) to 0
+#define TOGGLE_VAL_BIT(REG , BIT)   REG ^= (1 << BIT)        // Toggle the bit value (BIT) in the register (REG)
+#define READ_BIT_VAL(REG , BIT)     REG  & (1 << BIT)        // Read the bit value (BIT) in the register (REG) that might be 0 or 1 
 
 
 #endif 
